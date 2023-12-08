4f6b51c6facd03ed524c5b91071452bb65e0e700
Ronnel Gambong <ronnelgambong123@gmail.com>
2023-12-08 05:36:30 +0000

    Dec 8, 2023, 1:36 PM

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,14 @@
 const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+const navMenu = document.querySelector(".navLinks");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 });
+
+document.querySelector(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classlist.remove("active");
+    navMenu.classList.remove("active");
+  })
+);

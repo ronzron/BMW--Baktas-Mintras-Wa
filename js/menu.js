2f18e9b1dc486da963027d426b312f919e225d4f
Ronnel Gambong <ronnelgambong123@gmail.com>
2023-12-08 12:22:50 +0800

    Revert "Dec 8, 2023, 12:12 PM"
    This reverts commit 0094e21440df8cae6bf2b3c2e69d8a95c9de7cb2.

--- js/menu.js
+++ js/menu.js
@@ -1,15 +1,15 @@
-// const hamburgerMenu = document.getElementById('hamburgerMenu');
-// const navLinks = document.querySelector('.navLinks');
+const hamburgerMenu = document.getElementById('hamburgerMenu');
+const navLinks = document.querySelector('.navLinks');
 
-// hamburgerMenu.addEventListener('click', () => {
-//   navLinks.classList.toggle('active');
-//   hamburgerMenu.classList.toggle('active');
-// });
+hamburgerMenu.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+  hamburgerMenu.classList.toggle('active');
+});
 
-// // Close the menu and reset styles when the window is resized
-// window.addEventListener('resize', () => {
-//   if (window.innerWidth > 460) {
-//     navLinks.classList.remove('active');
-//     hamburgerMenu.classList.remove('active');
-//   }
-// });
+// Close the menu and reset styles when the window is resized
+window.addEventListener('resize', () => {
+  if (window.innerWidth > 460) {
+    navLinks.classList.remove('active');
+    hamburgerMenu.classList.remove('active');
+  }
+});
